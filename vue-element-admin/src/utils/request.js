266b812ac7a238a65a976c1898f4de7714098a67
zhoujie <1599887292@qq.com>
2019-04-28 14:29:49 +0800

    修改路径

--- vue-element-admin/src/utils/request.js
+++ vue-element-admin/src/utils/request.js
@@ -4,8 +4,8 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: '/api', // api 的 base_url
-  //baseURL:/jasonandjay\.com/.test(window.location.host)?'https://exam.jasonandjay.com/':'http://169.254.95.2:7001',
+  //baseURL: '/api', // api 的 base_url
+  baseURL:/jasonandjay\.com/.test(window.location.host)?'https://exam.jasonandjay.com/':'http://169.254.95.2:7001',
   //withCredentials: false, // 跨域请求时发送 cookies
 
   timeout: 5000 // request timeout
